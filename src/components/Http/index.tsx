8d15731f5e4460bf6860d0d43e69ffe1e9fa4ef5
Allen Zhang (张涛) <tzhangm@trip.com>
2022-06-16 13:27:33 +0800

    换回wp

--- src/components/Http/index.tsx
+++ src/components/Http/index.tsx
@@ -1,16 +1,10 @@
+import { Button } from "antd";
 import {useState} from "react";
-import Button from '@mui/material/Button';
 
 const Http = () => {
   const [count, setCount] = useState(0);
   return <div style={{fontStyle:'12px'}}>
-    <Button variant="contained">Contained</Button>
-    <Button variant="contained" disabled>
-      Disabled
-    </Button>
-    <Button variant="contained" href="#contained-buttons">
-      Link
-    </Button>
+<Button>s</Button>
   </div>;
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,9 @@ import App from "./App";
 import "./index.css";
 import "antd/dist/antd.min.css";
 import { BrowserRouter } from "react-router-dom";
-import {CssBaseline} from "@mui/material";
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>,
 );
