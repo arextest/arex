6ebfbbda5409470a68bf1900e3bfe9727eb6db19
onePone <pwang31@trip.com>
2022-07-20 11:26:51 +0800

    fix: inherit props

--- src/components/SmallTextButton/index.tsx
+++ src/components/SmallTextButton/index.tsx
@@ -2,11 +2,11 @@ import styled from "@emotion/styled";
 import { Button, ButtonProps } from "antd";
 
 const SmallTextButton = styled((props: ButtonProps) => (
-  <Button type="text" size="small">
+  <Button type="text" size="small" className={props.className} {...props}>
     {props.title}
   </Button>
 ))<{ title: string } & ButtonProps>`
-  color: ${(props) => props.theme.color.primary};
+  color: ${(props) => props.theme.color.primary}!important;
 `;
 
 export default SmallTextButton;
