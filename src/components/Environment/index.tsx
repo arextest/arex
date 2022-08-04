12ea78ae0288d032703bbcf79b232c4b2f18b4e8
onePone <pwang31@trip.com>
2022-08-04 14:54:17 +0800

    feat: add lint-staged

--- src/components/Environment/index.tsx
+++ src/components/Environment/index.tsx
@@ -1,36 +1,44 @@
-import { Button, Empty, Input, Spin, Tooltip, Tree } from 'antd';
-import { css } from '@emotion/react';
 import { DashOutlined, DownOutlined, MenuOutlined, PlusOutlined } from '@ant-design/icons';
-import  EnvironmentTitleRender  from './EnvironmentTitleRender'
-import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import { Button, Empty, Input, Spin, Tooltip, Tree } from 'antd';
 import type { DirectoryTreeProps } from 'antd/lib/tree';
+import { useEffect, useState } from 'react';
 
+import EnvironmentTitleRender from './EnvironmentTitleRender';
 
 export type Environmentprops = {
   activePane: () => void;
-  EnvironmentData: []|undefined;
+  EnvironmentData: [] | undefined;
   setMainBoxPanes: (p: any) => void;
   mainBoxPanes: any[];
   setMainBoxActiveKey: (p: any) => void;
-  activeKey:string;
-  setEnvironmentSelectedData:(p: any) => void;
-  fetchEnvironmentDatas:() => void;
-  nowEnvironment:string;
-  setCurEnvironment:(p: any) => void;
+  activeKey: string;
+  setEnvironmentSelectedData: (p: any) => void;
+  fetchEnvironmentDatas: () => void;
+  nowEnvironment: string;
+  setCurEnvironment: (p: any) => void;
 };
 
-const Environment = ({ setCurEnvironment,activePane,EnvironmentData,setMainBoxPanes,mainBoxPanes,setMainBoxActiveKey,activeKey,setEnvironmentSelectedData,fetchEnvironmentDatas,nowEnvironment }:Environmentprops) => {
+const Environment = ({
+  setCurEnvironment,
+  activePane,
+  EnvironmentData,
+  setMainBoxPanes,
+  mainBoxPanes,
+  setMainBoxActiveKey,
+  activeKey,
+  setEnvironmentSelectedData,
+  fetchEnvironmentDatas,
+  nowEnvironment,
+}: Environmentprops) => {
   const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
-  const [titleRadius,setTitleRadius]=useState<string>('');
+  const [titleRadius, setTitleRadius] = useState<string>('');
   const onSelect: DirectoryTreeProps['onSelect'] = (keys, info) => {
     if (keys.length > 0) {
       setSelectedKeys(keys);
     }
-    
-    if (
-      keys[0] &&
-      !mainBoxPanes.map((i) => i.key).includes(keys[0])
-    ) {
+
+    if (keys[0] && !mainBoxPanes.map((i) => i.key).includes(keys[0])) {
       const newPanes = [...mainBoxPanes];
       newPanes.push({
         title: info.node.envName,
@@ -45,48 +53,50 @@ const Environment = ({ setCurEnvironment,activePane,EnvironmentData,setMainBoxPa
     if (keys[0]) {
       setMainBoxActiveKey(keys[0]);
     }
-  }
-  useEffect(()=>{
-    setEnvironmentSelectedData(EnvironmentData?.filter(e=>e.id==activeKey))
+  };
+  useEffect(() => {
+    setEnvironmentSelectedData(EnvironmentData?.filter((e) => e.id == activeKey));
     setSelectedKeys([activeKey]);
-  },[activeKey])
+  }, [activeKey]);
   return (
     <div>
-      <div css={css`
-            display: flex;
-            justify-content: space-between;
-            margin-top: 10px;
-            margin-bottom: 10px;
-          `}>
-          <Tooltip placement='bottomLeft' title={'Create New'} mouseEnterDelay={0.5}>
-            <Button
-              css={css`
+      <div
+        css={css`
+          display: flex;
+          justify-content: space-between;
+          margin-top: 10px;
+          margin-bottom: 10px;
+        `}
+      >
+        <Tooltip placement='bottomLeft' title={'Create New'} mouseEnterDelay={0.5}>
+          <Button
+            css={css`
               margin-right: 5px;
             `}
-              icon={<PlusOutlined />}
-              type='text'
-              size='small'
-              onClick={() => {
-                activePane();
-              }}
-            />
-          </Tooltip>
-          <Input
-            disabled={true}
-            className={'environment-header-search'}
+            icon={<PlusOutlined />}
+            type='text'
             size='small'
-            placeholder=''
-            prefix={<MenuOutlined />}
+            onClick={() => {
+              activePane();
+            }}
           />
-        </div>
-        <Tree
+        </Tooltip>
+        <Input
+          disabled={true}
+          className={'environment-header-search'}
+          size='small'
+          placeholder=''
+          prefix={<MenuOutlined />}
+        />
+      </div>
+      <Tree
         blockNode={true}
         selectedKeys={selectedKeys}
-        fieldNames={{title:'envName',key:'id'}}
-        onMouseEnter={(e:any)=>{
+        fieldNames={{ title: 'envName', key: 'id' }}
+        onMouseEnter={(e: any) => {
           setTitleRadius(e.node.id);
         }}
-        onMouseLeave={()=>{
+        onMouseLeave={() => {
           setTitleRadius('');
         }}
         onSelect={onSelect}
@@ -94,7 +104,9 @@ const Environment = ({ setCurEnvironment,activePane,EnvironmentData,setMainBoxPa
         titleRender={(val) => (
           <EnvironmentTitleRender
             titleRadius={titleRadius}
-            updateDirectorytreeData={()=>{fetchEnvironmentDatas()}}
+            updateDirectorytreeData={() => {
+              fetchEnvironmentDatas();
+            }}
             val={val}
             nowEnvironment={nowEnvironment}
             setNowEnvironment={setCurEnvironment}
