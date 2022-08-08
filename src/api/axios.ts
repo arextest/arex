8e03a1d5ae974f5fd9a6a3944764c313d477c816
张涛 <tzhangm@trip.com>
2022-08-08 20:50:20 +0800

    fix: upd

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -31,7 +31,8 @@ service.interceptors.response.use((response) => {
   // 只要有接口鉴权失败就重定向到/login
   if (data.responseStatusType.responseCode === 2){
     localStorage.clear()
-    window.location.href = '/login'
+    // TODO这里需要确定responseCode的类型
+    // window.location.href = '/login'
   }
   return data
 }, err)
