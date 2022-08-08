a36a8da28f580ccc76d8927083dd5b7078ece049
张涛 <tzhangm@trip.com>
2022-08-08 19:09:59 +0800

    chore: 添加鉴权

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from '../api/axios';
 export class LoginService {
   static sendVerifyCodeByEmail(email: string) {
     return axios.get(`/api/login/getVerificationCode/${email}`);
   }
   static loginVerify(params: { userName: string; verificationCode: string }) {
-    return axios.post(`/api/login/verify`, params);
+    return axios.post(`/api/verify`, params);
   }
 }
