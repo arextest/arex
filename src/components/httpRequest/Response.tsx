835e3ec2590b59398b99de8f7c63c001a54ceac9
pengfei.li <pengfei.li@trip.com>
2022-08-10 15:12:55 +0800

    feat: text

--- src/components/httpRequest/Response.tsx
+++ src/components/httpRequest/Response.tsx
@@ -3,10 +3,9 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import CodeMirror from '@uiw/react-codemirror';
 import { Tabs, List, Progress } from 'antd';
-import { FC, useEffect, useMemo } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 
 import { useStore } from '../../store';
-import { color } from '../../utils/chart';
 import FormTable, { getColumns } from './FormTable';
 const { TabPane } = Tabs;
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
@@ -21,6 +20,18 @@ const StatusWrapper = styled.div`
   }
 `;
 
+const TestError = styled.div`
+  text-align: center;
+  & > div:first-of-type {
+    color: #737373;
+  }
+  & > div:nth-of-type(2) {
+    color: #A3A3A3;
+    margin-top: 10px;
+  }
+
+`;
+
 const Response: FC<{
   res: any;
   status: { code: number; text: string };
@@ -28,6 +39,7 @@ const Response: FC<{
   size?: number;
   responseHeaders?: object;
   TestResult?: [];
+  isTestResult:boolean;
 }> = (props) => {
   const onChange = (key: string) => {
     console.log(key);
@@ -49,8 +61,13 @@ const Response: FC<{
     console.log(headers);
   }, [headers]);
 
+  const [testPass,setTestPass] = useState<number[]>([]);
   useEffect(() => {
-    console.log(props.TestResult);
+    const arrPass:number[]=[];
+    props.TestResult&&props.TestResult.map((e:any)=>{
+      arrPass.push(e.expectResults.filter((a:any)=>a.status=='pass').length);
+    })
+    setTestPass(arrPass);
   }, [props.TestResult]);
   return (
     <>
@@ -98,26 +115,27 @@ const Response: FC<{
           />
         </TabPane>
         <TabPane tab='Test Results' key='4'>
-          {props.TestResult?.map((e: any, i) => (
+          {props.isTestResult?props.TestResult?.map((e: any, i) => (
             <List
+            key={i}
               size='large'
-              style={{ marginBottom: '20px' }}
+              css={css`margin-bottom: 10px `}
               header={
                 <div>
-                  <div style={{ marginTop: '-10px', fontWeight: 'bold' }}>{e.descriptor}</div>
-                  <div style={{ marginTop: '10px' }}>
+                  <div css={css`margin-top: -10px; font-weight: bold; `}>{e.descriptor}</div>
+                  <div css={css`margin-top: 10px `}>
                     <Progress
                       strokeColor={'#EF4444'}
                       width={20}
                       strokeWidth={20}
-                      percent={64}
-                      success={{ percent: 41, strokeColor: '#10B981' }}
+                      percent={100}
+                      success={{ percent: Math.round((testPass[i]/e.expectResults.length)*100), strokeColor: '#10B981' }}
                       type='circle'
                       showInfo={false}
                     />
-                    <span style={{ marginLeft: '10px' }}>
-                      failing,{e.expectResults.length} successful,out of {e.expectResults.length}{' '}
-                      tests
+                    <span css={css`margin-left: 10px `}>
+                    {e.expectResults.length-testPass[i]?<span css={css`color: #EF4444;`}>{e.expectResults.length-testPass[i]} failing, </span>:<></>}
+                    {testPass[i]?<span css={css`color: #10B981;`}>{testPass[i]} successful, </span>:<></>}out of {e.expectResults.length} tests
                     </span>
                   </div>
                 </div>
@@ -127,15 +145,19 @@ const Response: FC<{
               renderItem={(item: any, i) => (
                 <List.Item key={i}>
                   {item.status == 'pass' ? (
-                    <CheckOutlined style={{ color: '#10B981', marginRight: '15px' }} />
+                    <CheckOutlined css={css`color: #10B981; margin-right: 15px`}/>
                   ) : (
-                    <CloseOutlined style={{ color: '#EF4444', marginRight: '15px' }} />
+                    <CloseOutlined css={css`color: #EF4444; margin-right: 15px`}/>
                   )}
                   {item.message}——{item.status == 'pass' ? '测试成功' : '测试失败'}
                 </List.Item>
               )}
             />
-          ))}
+          )):<TestError>
+              <img src={'https://hoppscotch.io/images/states/light/youre_lost.svg'}></img>
+              <div>无法执行请求脚本</div>
+              <div>测试脚本似乎有一个错误，请修复错误并再次运行测试</div>
+            </TestError>}
         </TabPane>
       </Tabs>
     </>
