e1c7de819986936a662dbe0a92d54e992bdd769a
onePone <pwang31@trip.com>
2022-09-26 17:19:08 +0800

    feat: nodeDifferences

--- src/components/replay/Setting/NodeDifferences.tsx
+++ src/components/replay/Setting/NodeDifferences.tsx
@@ -1,61 +1,57 @@
-import { Card, Tree } from 'antd';
+import { Card, Col, Row, Tree } from 'antd';
 import { TreeProps } from 'antd/es';
 import { DataNode } from 'antd/lib/tree';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-const treeData: DataNode[] = [
-  {
-    title: 'parent 1',
-    key: '0-0',
-    children: [
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-        disabled: true,
-        children: [
-          {
-            title: 'leaf',
-            key: '0-0-0-0',
-            disableCheckbox: true,
-          },
-          {
-            title: 'leaf',
-            key: '0-0-0-1',
-          },
-        ],
-      },
-      {
-        title: 'parent 1-1',
-        key: '0-0-1',
-        children: [{ title: 'sss', key: '0-0-1-0' }],
-      },
-    ],
-  },
-];
+const data = { parent1: { child1: { bar: '1' }, child2: '2' }, parent2: { child3: '1' } };
 
 const NodeDifferences: FC = () => {
+  const [checkedNodes, setCheckedNodes] = useState([]);
+
   const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
   };
 
   const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
-    console.log('onCheck', checkedKeys, info);
+    console.log('onCheck', checkedKeys.checked, info);
+    setCheckedNodes(info.checkedNodes.map((node) => node.key));
   };
 
+  function getNodes(object: object, basePath = ''): DataNode[] {
+    return Object.entries(object).map(([key, value]) =>
+      value && typeof value === 'object'
+        ? { title: key, key: basePath + key + '/', children: getNodes(value, key + '/') }
+        : { title: key, key: basePath + key + '/', value },
+    );
+  }
+
   return (
-    <>
-      <span>NodeDifferences</span>
-      <Card>
-        <Tree
-          defaultExpandedKeys={['0-0-0', '0-0-1']}
-          defaultSelectedKeys={['0-0-0', '0-0-1']}
-          defaultCheckedKeys={['0-0-0', '0-0-1']}
-          onSelect={onSelect}
-          onCheck={onCheck}
-          treeData={treeData}
-        />
-      </Card>
-    </>
+    <Row>
+      <Col span={12}>
+        <h3>Global</h3>
+        <div>Ignored Nodes:</div>
+        {checkedNodes.map((node, i) => (
+          <div key={i}>{node}</div>
+        ))}
+
+        <br />
+
+        <h3>Interfaces</h3>
+      </Col>
+
+      <Col span={12}>
+        <Card title='Global (click node to ignore)'>
+          <Tree
+            checkable
+            checkStrictly
+            onSelect={onSelect}
+            onCheck={onCheck}
+            treeData={getNodes(data)}
+            filterTreeNode={(node) => node.key === 'parent1/'}
+          />
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
