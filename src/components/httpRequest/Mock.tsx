c299a1ab156dc9b4fb5898c6899d4ba5fa60242e
tzhangm <tzhangm@trip.com>
2022-10-17 20:06:44 +0800

    feat: 固化用例展示

--- src/components/httpRequest/Mock.tsx
+++ src/components/httpRequest/Mock.tsx
@@ -1,104 +1,127 @@
+import { css } from '@emotion/react';
 import { useMount } from 'ahooks';
-import { Col, Input, Row, Table } from 'antd';
+import { Card, Col, Divider, Input, Row } from 'antd';
 import { useState } from 'react';
 
 import request from '../../api/axios';
 import { tryParseJsonString } from '../../helpers/utils';
 
-const Mock = () => {
+const Mock = ({ recordId }) => {
   const [dataSource, setDataSource] = useState([]);
-
-  const columns = [
-    {
-      title: 'appId',
-      dataIndex: 'appId',
-    },
-    {
-      title: 'createTime',
-      dataIndex: 'createTime',
-    },
-    {
-      title: 'method',
-      dataIndex: 'method',
-    },
-    {
-      title: 'path',
-      dataIndex: 'path',
-    },
-    {
-      title: 'pattern',
-      dataIndex: 'pattern',
-    },
-    {
-      title: 'recordId',
-      dataIndex: 'recordId',
-    },
-
-    {
-      title: 'request',
-      dataIndex: 'request',
-    },
-    //
-    {
-      title: 'requestHeaders',
-      dataIndex: 'requestHeaders',
-    },
-    //
-    // {
-    //   title: 'response',
-    //   dataIndex: 'response',
-    // },
-    {
-      title: 'responseHeaders',
-      dataIndex: 'responseHeaders',
-    },
-    // {
-    //   title: '年龄',
-    //   dataIndex: 'age',
-    //   key: 'age',
-    // },
-    // {
-    //   title: '住址',
-    //   dataIndex: 'address',
-    //   key: 'address',
-    // },
-  ];
   useMount(() => {
     request
       .post(`/storage/frontEnd/record/queryRecord`, {
-        recordId: 'AREX-172-17-0-2-72773767300242',
+        recordId: recordId,
         categoryTypes: 0,
       })
       .then((res) => {
-        const arr = [];
+        const record = [];
         Object.keys(res.recordResult).forEach((item) => {
-          arr.push({
-            key: item,
-            ...tryParseJsonString(res.recordResult[item]),
-          });
+          const getRequestKey = {
+            '4': {
+              operation: 'clusterName',
+              request: 'redisKey',
+              response: 'response',
+            },
+            '5': {
+              operation: 'clazzName',
+              request: 'operationKey',
+              response: 'response',
+            },
+            '6': {
+              operation: 'service',
+              request: 'request',
+              response: 'response',
+            },
+            '7': {
+              operation: 'expCode',
+              request: 'version',
+              response: 'response',
+            },
+            '13': {
+              operation: 'url',
+              request: 'request',
+              response: 'response',
+            },
+            '14': {
+              operation: 'dbName',
+              request: 'sql',
+              response: 'response',
+            },
+            '15': {
+              operation: 'path',
+              request: 'request',
+              response: 'response',
+            },
+          };
+          for (let i = 0; i < res.recordResult[item].length; i++) {
+            const recordResult = tryParseJsonString(res.recordResult[item][i]);
+            record.push({
+              key: item,
+              ...recordResult,
+              request: recordResult[getRequestKey[item].request],
+              operation: recordResult[getRequestKey[item].operation],
+              requestKey: getRequestKey[item].request,
+              operationKey: getRequestKey[item].operation,
+            });
+          }
         });
-        setDataSource(arr.filter((i) => i.appId));
+        setDataSource(record);
       });
   });
   return (
-    <div>
+    <div
+      css={css`
+        max-height: 500px;
+        overflow-y: auto;
+      `}
+    >
       {JSON.stringify(dataSource.appId)}
-      {/*<Table dataSource={dataSource} columns={columns} />*/}
-
       {dataSource.map((i) => {
         return (
-          <div>
-            <p>{i.path}</p>
-
-            <Row>
-              <Col span={12}>
-                <Input.TextArea autoSize value={i.request} />
+          <Card style={{ margin: '0 0 10px 0' }}>
+            <p
+              css={css`
+                font-weight: bolder;
+                margin-bottom: 10px;
+                font-size: 18px;
+              `}
+            >
+              {i.operationKey}:{i.operation}
+            </p>
+            <Divider />
+            <Row gutter={16}>
+              <Col span={12} style={{ display: 'flex', flexDirection: 'column' }}>
+                <div
+                  css={css`
+                    font-weight: bolder;
+                    margin-bottom: 10px;
+                    font-size: 16px;
+                  `}
+                >
+                  {i.requestKey}
+                </div>
+                <Input.TextArea
+                  style={{ height: '100%', minHeight: '200px', flex: '1' }}
+                  readOnly
+                  autoSize
+                  value={i.request}
+                />
               </Col>
               <Col span={12}>
-                <Input.TextArea autoSize value={i.response} />
+                <div
+                  css={css`
+                    font-weight: bolder;
+                    margin-bottom: 10px;
+                    font-size: 16px;
+                  `}
+                >
+                  response
+                </div>
+                <Input.TextArea style={{ height: '100%' }} readOnly autoSize value={i.response} />
               </Col>
             </Row>
-          </div>
+          </Card>
         );
       })}
     </div>
