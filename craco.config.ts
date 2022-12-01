2df647e758b76e8a39da38f5b99e9ccdb212ede6
tzhangm <tzhangm@trip.com>
2022-12-01 15:47:31 +0800

    fix: migrate update

--- craco.config.ts
+++ craco.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
@@ -9,4 +11,20 @@ export default {
     port: 8888,
     open: false,
   },
+  webpack: {
+    alias: {
+
+    },
+    // 以下代码！！！  与alias或babel同级
+    configure:(webpackConfig: { output: any; }, {env, paths}: any) => {
+      // 修改build的生成文件名称
+      paths.appBuild = 'dist';
+      webpackConfig.output ={
+        ...webpackConfig.output,
+        path:path.resolve(__dirname,'dist'),
+        publicPath:'/'
+      }
+      return webpackConfig;
+    }
+  }
 };
