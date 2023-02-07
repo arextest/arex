05cd8a1c2bc8b3f56f7f2cdc97747f0289513132
onePone <pwang31@trip.com>
2023-02-07 15:40:01 +0800

    fix: nodesIgnore rawResponse

--- src/components/appSetting/NodesIgnore/index.tsx
+++ src/components/appSetting/NodesIgnore/index.tsx
@@ -35,6 +35,8 @@ const SettingNodesIgnore: FC<SettingNodeIgnoreProps> = (props) => {
   const { message } = App.useApp();
   const { t } = useTranslation(['components', 'common']);
 
+  const [rawResponse, setRawResponse] = useState<string>();
+
   const [checkedNodesData, setCheckedNodesData] = useImmer<{
     operationId?: OperationId<'Global'>;
     operationName?: string;
@@ -154,6 +156,9 @@ const SettingNodesIgnore: FC<SettingNodeIgnoreProps> = (props) => {
       onBefore() {
         setInterfaceResponse();
       },
+      onSuccess(res) {
+        setRawResponse(tryPrettierJsonString(res?.operationResponse || ''));
+      },
     },
   );
   const interfaceResponseParsed = useMemo<{ [key: string]: any }>(() => {
@@ -189,8 +194,8 @@ const SettingNodesIgnore: FC<SettingNodeIgnoreProps> = (props) => {
    * 保存某个 interface 的 response
    * @param value
    */
-  const handleResponseSave = (value?: string) => {
-    const parsed = value && tryParseJsonString(value, 'Invalid JSON');
+  const handleResponseSave = () => {
+    const parsed = rawResponse && tryParseJsonString(rawResponse, 'Invalid JSON');
     if (!!activeOperationInterface?.id && parsed) {
       updateInterfaceResponse({
         id: activeOperationInterface.id,
@@ -301,7 +306,8 @@ const SettingNodesIgnore: FC<SettingNodeIgnoreProps> = (props) => {
               />
             ) : (
               <ResponseRaw
-                value={tryPrettierJsonString(interfaceResponse?.operationResponse || '')}
+                value={rawResponse}
+                onChange={setRawResponse}
                 onSave={handleResponseSave}
                 onCancel={handleCancelEditResponse}
               />
