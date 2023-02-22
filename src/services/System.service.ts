6ccf890856289cf80ca9a09dab45d3cfd448cc3b
onePone <pwang31@trip.com>
2023-02-22 20:44:24 +0800

    feat: logs time filter

--- src/services/System.service.ts
+++ src/services/System.service.ts
@@ -2,7 +2,14 @@ import request from '../helpers/api/axios';
 import { QueryLogsReq, QueryLogsRes } from './System.type';
 
 export class SystemService {
-  static async queryLogs(params: QueryLogsReq) {
+  static async queryLogs(params: {
+    previousId: string | undefined;
+    level: string;
+    pageSize: number;
+    tags: { 'app-type'?: string };
+    startTime?: number;
+    endTime?: number;
+  }) {
     const res = await request.post<QueryLogsRes>(`/report/logs/query`, params);
     return res.body.logs;
   }
