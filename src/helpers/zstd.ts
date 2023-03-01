ff5e87421132c47358b811e9baf46b70d6e3dcfe
tzhangm <tzhangm@trip.com>
2023-03-01 15:09:44 +0800

    fix: update zstd function

--- src/helpers/zstd.ts
+++ src/helpers/zstd.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 import { Zstd } from '@hpcc-js/wasm/zstd';
-
+import { Base64 } from 'js-base64';
 export async function compressedData(str: string) {
   const zstd = await Zstd.load();
   const data = new TextEncoder().encode(str);
   const compressed_data = zstd.compress(data);
-  return window.btoa(compressed_data);
+  return Base64.fromUint8Array(compressed_data);
 }
