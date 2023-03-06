26f8663ba4620be5aff9fa556b431574f2e3ca4d
tzhangm <tzhangm@trip.com>
2023-03-06 10:47:39 +0800

    feat: rel/1.0.6_vite

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+// import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+  server: {
+    port: 8001,
+    proxy: {
+      '/report': {
+        target: 'http://10.5.153.1:8090',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/report', '/api'),
+      },
+      '/schedule': {
+        target: 'http://10.5.153.1:8092',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/schedule', '/api'),
+      },
+      '/storage': {
+        target: 'http://10.5.153.1:8093',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/storage', '/api'),
+      },
+    },
+  },
+});
