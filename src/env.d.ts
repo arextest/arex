3a481db1854782ad7f17eba770fb7f30c29d856b
tzhangm <tzhangm@trip.com>
2023-03-10 16:57:38 +0800

    fix: add arex desktop agent

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,5 +10,6 @@ declare global {
   interface Window {
     __AREX_EXTENSION_INSTALLED__: boolean; // 是否安装了arex-chrome-extension
     __AREX_EXTENSION_VERSION__: string; // arex-chrome-extension 最新版本号
+    __AREX_DESKTOP_AGENT__: boolean; //是否安装了arex桌面代理
   }
 }
