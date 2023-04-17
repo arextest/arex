8242a8dd571074978e9e4e6801dd65c97173392a
tzhangm <tzhangm@trip.com>
2023-04-17 11:14:15 +0800

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,18 @@ import 'allotment/dist/style.css';
 import 'antd/dist/reset.css';
 import 'vanilla-jsoneditor/themes/jse-theme-dark.css';
 
+import * as Sentry from '@sentry/react';
 import { ClickToComponent } from 'click-to-react-component';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+if (import.meta.env.MODE !== 'development') {
+  Sentry.init({
+    dsn: 'https://bd34e6a640f44ebdad2e05fd993474bd@sentry-performance.ctrip.com/3',
+    integrations: [new Sentry.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
