3b0c0e47e5aa56ff72ea9225f4e95715e43abd31
onePone <pwang31@trip.com>
2023-04-20 17:05:34 +0800

    feat: component ArexFooter

--- packages/arex-core/src/layout/ArexFooter.tsx
+++ packages/arex-core/src/layout/ArexFooter.tsx
@@ -1,15 +1,75 @@
-import { css } from '@emotion/react';
-import { theme } from 'antd';
-const { useToken } = theme;
-const ArexFooter = () => {
-  const token = useToken();
+import { CodeOutlined } from '@ant-design/icons';
+import styled from '@emotion/styled';
+import { Button, Typography } from 'antd';
+import React, { FC, useEffect, useRef } from 'react';
+import VConsole from 'vconsole';
+
+import { CheckOrCloseIcon } from '../components';
+import { useArexCoreConfig } from '../hooks';
+
+const FooterWrapper = styled.div`
+  height: 26px;
+  width: 100%;
+  padding: 0 8px;
+  display: flex;
+  justify-content: space-between;
+  z-index: 1000;
+  border-top: 1px solid ${(props) => props.theme.colorBorder};
+  .ant-typography {
+    line-height: 24px;
+    font-size: 12px;
+  }
+  .ant-btn-link {
+    .ant-typography-secondary {
+      transition: color 0.25s ease;
+      &:hover {
+        color: ${(props) => props.theme.colorTextSecondary};
+      }
+    }
+  }
+`;
+
+const ArexFooter: FC = () => {
+  const { theme } = useArexCoreConfig();
+  const vConsole = useRef<VConsole>();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      vConsole.current = new VConsole({
+        theme,
+        log: {
+          showTimestamps: true,
+        },
+        defaultPlugins: [],
+        pluginOrder: ['logs'],
+      });
+      vConsole.current.hideSwitch();
+
+      return () => vConsole.current?.destroy();
+    }
+  }, []);
+
   return (
-    <div
-      css={css`
-        height: 33px;
-        border-top: 1px solid ${token.token.colorBorder};
-      `}
-    ></div>
+    <FooterWrapper>
+      {/* left */}
+      <div>
+        <Button type='link' size='small' onClick={() => vConsole.current?.show()}>
+          <Typography.Text type='secondary'>
+            <CodeOutlined /> Console
+          </Typography.Text>
+        </Button>
+      </div>
+
+      {/* right */}
+      <div>
+        <span>
+          {/* @ts-ignore */}
+          <CheckOrCloseIcon size={12} checked={window.__AREX_EXTENSION_INSTALLED__} />
+          <Typography.Text type='secondary'>Browser Agent</Typography.Text>
+        </span>
+      </div>
+    </FooterWrapper>
   );
 };
+
 export default ArexFooter;
