ecd8dee31218a87d86929dab41ec4f7685736088
tzhangm <tzhangm@trip.com>
2023-05-11 14:13:04 +0800

    feat: add server.js

---
+++ packages/arex/server.js
@@ -0,0 +1,70 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const history = require('connect-history-api-fallback');
+const express = require('express');
+const app = express();
+const e = {
+  SERVICE_REPORT_URL:"http://10.5.153.1:8090",
+  SERVICE_SCHEDULE_URL:"http://10.5.153.1:8092",
+  SERVICE_STORAGE_URL:"http://10.5.153.1:8093",
+  SERVICE_NODE_URL:"http://10.5.153.1:10001"
+}
+app.use(
+  '/report',
+  createProxyMiddleware({
+    target: e.SERVICE_REPORT_URL,
+    changeOrigin: true,
+    pathRewrite: { '/report': '/api' },
+  }),
+);
+
+app.use(
+  '/schedule',
+  createProxyMiddleware({
+    target: e.SERVICE_SCHEDULE_URL,
+    changeOrigin: true,
+    pathRewrite: { '/schedule': '/api' },
+  }),
+);
+
+app.use(
+  '/storage',
+  createProxyMiddleware({
+    target: e.SERVICE_STORAGE_URL,
+    changeOrigin: true,
+    pathRewrite: { '/storage': '/api' },
+  }),
+);
+
+app.use(
+  '/node',
+  createProxyMiddleware({
+    target: e.SERVICE_NODE_URL,
+    changeOrigin: true,
+    pathRewrite: { '/node': '/' },
+  }),
+);
+
+
+// 健康检查
+app.get('/vi/health', (req, res) => {
+  res.end(`365ms`);
+});
+// storage
+app.get('/env', (req, res) => {
+  res.send({
+    SERVICE_REPORT_URL: e.SERVICE_REPORT_URL,
+    SERVICE_CONFIG_URL: e.SERVICE_CONFIG_URL,
+    SERVICE_SCHEDULE_URL: e.SERVICE_SCHEDULE_URL,
+    SERVICE_STORAGE_URL: e.SERVICE_STORAGE_URL,
+    SERVICE_NODE_URL:e.SERVICE_NODE_URL
+  });
+});
+
+app.use(history()); // 这里千万要注意，要在static静态资源上面
+// 托管静态文件
+app.use(express.static('dist'));
+
+// 监听8080端口
+app.listen(8080, function () {
+  console.log('hi');
+});
