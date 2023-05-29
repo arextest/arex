3328fced4f657e7426b84a99fa3b80d80c14ccd5
tzhangm <tzhangm@trip.com>
2023-05-29 10:18:32 +0800

    feat: update monaco version

--- packages/arex/src/userWorker.ts
+++ packages/arex/src/userWorker.ts
@@ -4,6 +4,7 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
 // @ts-ignore
 self.MonacoEnvironment = {
   getWorker(_: any, label: string) {
