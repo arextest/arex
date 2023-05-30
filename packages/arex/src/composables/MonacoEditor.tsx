107532b5db17805e8d9278eb703ed6a22997e234
onePone <pwang31@trip.com>
2023-05-30 15:42:09 +0800

    feat: importYaml use Monaco

--- packages/arex/src/composables/MonacoEditor.tsx
+++ packages/arex/src/composables/MonacoEditor.tsx
@@ -1,19 +1,12 @@
-import { css } from '@emotion/react';
-import { FC, useRef } from 'react';
+import React, { FC, useRef } from 'react';
 
 import { Options, useMonaco } from './monaco';
 
 const MonacoEditor: FC<{ value: string; option: Options }> = ({ value, option }) => {
   const ref = useRef(null);
   useMonaco(ref, value, option);
-  return (
-    <div
-      css={css`
-        height: 360px;
-      `}
-      ref={ref}
-    ></div>
-  );
+
+  return <div style={{ height: option.height || '360px' }} ref={ref} />;
 };
 
 export default MonacoEditor;
