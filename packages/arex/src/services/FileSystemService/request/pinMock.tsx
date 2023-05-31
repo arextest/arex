5af56d8bfb9d6b55e9d1939d34c92ed6127714de
tzhangm <tzhangm@trip.com>
2023-05-31 21:20:46 +0800

    feat: update batch run

--- packages/arex/src/services/FileSystemService/request/pinMock.tsx
+++ packages/arex/src/services/FileSystemService/request/pinMock.tsx
@@ -2,7 +2,7 @@
 
 import { request } from '@/utils';
 
-export async function pinMock(params:any) {
+export async function pinMock(params: any) {
   const res = await request.post<{ success: boolean }>(`/report/filesystem/pinMock`, params);
   return res.body.success;
 }
