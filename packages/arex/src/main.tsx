2ae880deb3d02959ee2922fb6ee65fb543d6b0f9
Allen Zhang (张涛) <wr_zhang25@163.com>
2023-06-25 15:31:37 +0800

    feat: update sentry

--- packages/arex/src/main.tsx
+++ packages/arex/src/main.tsx
@@ -6,12 +6,19 @@ import 'dayjs/locale/zh-cn';
 import './helpers/adapter';
 import './assets/css/index.css';
 
+import * as Sentry from '@sentry/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-
+if (import.meta.env.MODE !== 'development') {
+  Sentry.init({
+    dsn: 'https://93269396efbd4d9eb84c60aabe220cb5@o4505063820034048.ingest.sentry.io/4505418776510464',
+    integrations: [new Sentry.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <App />
--- packages/arex/vite.config.ts
+++ packages/arex/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -28,7 +29,18 @@ export default defineConfig({
         return modules;
       },
     },
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: 'arextest',
+      project: 'arex-ui',
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: '46c8ead9fe034d13a77de7eb2e9a3294d007aec9a8e740c1ad94f67b75bcc41d',
+    }),
   ],
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
   resolve: {
     alias: {
       '@': path.resolve('./src'),
