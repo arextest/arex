fab11553f6b7c58a3b164df437f571c2cfab2b17
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2023-07-27 14:22:14 +0800

    fix: update new request

--- packages/arex/src/menus/Collection/Collection.tsx
+++ packages/arex/src/menus/Collection/Collection.tsx
@@ -95,7 +95,7 @@ const Collection: ArexMenuFC = (props) => {
 
   const filterTreeData = useMemo(
     // TODO searchValue 裁剪树形数据
-    // filter nodeType === 3 是为了隐藏第一层级只显示文件夹类型
+    // filter nodeType === 3 是为了隐藏第一层级只显示文件夹类型（由于新增请求时会新增一个临时的request到树形目录的第一层）
     () => collectionsTreeData.filter((item) => item.nodeType === 3),
     [collectionsTreeData, searchValue],
   );
