80b8e3318dad30d09a0dd699eb0b657852a96d7d
onePone <pwang31@trip.com>
2023-08-01 19:09:11 +0800

    feat: arex pane drag

--- packages/arex-core/src/utils/index.ts
+++ packages/arex-core/src/utils/index.ts
@@ -3,5 +3,6 @@ export { ArexPaneManager } from './ArexPaneManager';
 export * from './json';
 export * from './localStorage';
 export * from './url';
+export { arrayMove } from '@dnd-kit/sortable';
 export { css, useTheme } from '@emotion/react';
 export { default as styled } from '@emotion/styled';
