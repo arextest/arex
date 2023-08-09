fa9df66e3d491511adc661afb53efe03c34c0a74
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2023-08-09 13:47:34 +0800

    feat: update GitHubStarButton

--- packages/arex-core/src/components/GithubStarButton.tsx
+++ packages/arex-core/src/components/GithubStarButton.tsx
@@ -7,12 +7,12 @@ const GitHubStarButton: FC<{ theme: Theme }> = ({ theme }) => {
   return (
     <div style={{ height: '22px', lineHeight: '31px', margin: '0 16px' }}>
       <GitHubButton
-        data-text={'Star'}
-        aria-label={'Star Arex on GitHub'}
+        data-text={'Follower'}
+        aria-label={'Follower Arex on GitHub'}
         data-show-count={true}
         data-color-scheme={theme}
-        title={'Star Arex'}
-        href='https://github.com/arextest/arex'
+        title={'Follower Arex'}
+        href='https://github.com/arextest'
       />
     </div>
   );
