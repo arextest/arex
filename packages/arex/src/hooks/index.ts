7a42ed8222072603b9d653b881d2b9ff95d0f745
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2023-08-17 15:57:40 +0800

    Trace (#403)
    * feat: add trace
    
    * feat: remove sentry & useTrace
    
    * feat: update trace url
    
    ---------
    
    Co-authored-by: onePone <pwang31@trip.com>

--- packages/arex/src/hooks/index.ts
+++ packages/arex/src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useCheckChrome } from './useCheckChrome';
 export { default as useColorPrimary } from './useColorPrimary';
 export { default as useInit } from './useInit';
 export { default as useNavPane } from './useNavPane';
+export { default as useTrace } from './useTrace';
--- packages/arex/src/vite-env.d.ts
+++ packages/arex/src/vite-env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="vite/client" />
+import { Pane } from '@arextest/arex-core';
 import { MessageInstance } from 'antd/es/message/interface';
-export {};
+
+import { AREX_OPEN_NEW_PANEL } from '@/constant';
+
 declare global {
   interface Window {
     monaco: any;
@@ -8,5 +11,10 @@ declare global {
     __AREX_EXTENSION_INSTALLED__: boolean; // 是否安装了arex-chrome-extension
     __AREX_EXTENSION_VERSION__: string; // arex-chrome-extension 最新版本号
     __AREX_DESKTOP_AGENT__: boolean; //是否安装了arex桌面代理
+  }
+
+  // custom event type
+  interface WindowEventMap {
+    [AREX_OPEN_NEW_PANEL]: CustomEvent<Pane>;
   }
 }
