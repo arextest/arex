a2bcd0fc2854d5dba3744a683c52cf93a5b39ecf
onePone <pwang31@trip.com>
2023-08-31 14:13:16 +0800

    fix: appSetting button i18n

--- packages/arex/src/panes/Replay/AppTitle.tsx
+++ packages/arex/src/panes/Replay/AppTitle.tsx
@@ -82,7 +82,7 @@ const TitleWrapper = styled(
           <TooltipButton
             size='small'
             type='text'
-            title={t('replay.setting')}
+            title={t('replay.appSetting')}
             icon={<SettingOutlined />}
             onClick={props.onSetting}
           />
