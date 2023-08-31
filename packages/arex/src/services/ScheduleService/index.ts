7304558e9cd85f4b6d8740446e75099a6e22ef86
Jesse Rodriguez <54310591+1pone@users.noreply.github.com>
2023-08-31 12:41:52 +0800

    feat: replay plan retry (#431)
    Co-authored-by: onePone <pwang31@trip.com>

--- packages/arex/src/services/ScheduleService/index.ts
+++ packages/arex/src/services/ScheduleService/index.ts
@@ -2,4 +2,5 @@ export * from './createPlan';
 export * from './queryDiffMsgById';
 export * from './queryLogEntity';
 export * from './queryLogs';
+export * from './reRunPlan';
 export * from './stopPlan';
