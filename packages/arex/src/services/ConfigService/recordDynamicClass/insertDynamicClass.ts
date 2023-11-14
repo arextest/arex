3e8c11a9d303e687ca89b14cd3fa112eeac0d9d9
Jesse Rodriguez <54310591+1pone@users.noreply.github.com>
2023-11-14 19:57:47 +0800

    Feat record key formula (#538)
    * fix: init request url parse
    
    * fix: init request url parse
    
    * feat: exclusions noise reduce
    
    * feat: diff denoise check all
    
    * feat: record dynamic classes key formula
    
    ---------
    
    Co-authored-by: onePone <pwang31@trip.com>

--- packages/arex/src/services/ConfigService/recordDynamicClass/insertDynamicClass.ts
+++ packages/arex/src/services/ConfigService/recordDynamicClass/insertDynamicClass.ts
@@ -1,13 +1,7 @@
+import { DynamicClass } from '@/services/ConfigService';
 import { request } from '@/utils';
 
-export interface InsertDynamicClassReq {
-  appId: string;
-  fullClassName: string;
-  methodName?: string;
-  parameterTypes?: string | null;
-  configType: number;
-}
-export async function insertDynamicClass(params: InsertDynamicClassReq) {
+export async function insertDynamicClass(params: DynamicClass) {
   const res = await request.post<boolean>('/report/config/dynamicClass/modify/INSERT', params);
   return res.body;
 }
--- packages/arex/src/services/ConfigService/recordDynamicClass/updateDynamicClass.ts
+++ packages/arex/src/services/ConfigService/recordDynamicClass/updateDynamicClass.ts
@@ -1,14 +1,7 @@
+import { DynamicClass } from '@/services/ConfigService';
 import { request } from '@/utils';
 
-export type UpdateSettingReqInsert = {
-  id: string;
-  appId: string;
-  fullClassName: string;
-  methodName?: string;
-  parameterTypes?: string | null;
-};
-
-export async function updateDynamicClass(params: UpdateSettingReqInsert) {
+export async function updateDynamicClass(params: DynamicClass) {
   const res = await request.post<boolean>('/report/config/dynamicClass/modify/UPDATE', params);
   return res.body;
 }
