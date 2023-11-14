63969cb7297e34b8f81507aa33d257e314ca5afd
Jesse Rodriguez <54310591+1pone@users.noreply.github.com>
2023-11-14 19:58:00 +0800

    feat: batchRun progress (#521)
    * feat: batchRun progress
    
    * feat: batchrun step progress
    
    * feat: batchRun status blockMap
    
    * feat: update arex-request version
    
    ---------
    
    Co-authored-by: onePone <pwang31@trip.com>

--- packages/arex-request/src/components/Response/TestResult.tsx
+++ packages/arex-request/src/components/Response/TestResult.tsx
@@ -1,5 +1,5 @@
-import { css, EmptyWrapper, FlexCenterWrapper } from '@arextest/arex-core';
-import { Tag, theme, Typography } from 'antd';
+import { css, EmptyWrapper } from '@arextest/arex-core';
+import { Space, Tag, theme, Typography } from 'antd';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -7,54 +7,53 @@ import { ArexTestResult } from '../../types';
 
 const TestResult: FC<{ testResult?: ArexTestResult[] }> = ({ testResult }) => {
   const { token } = theme.useToken();
+
   const { t } = useTranslation();
 
   return (
-    <FlexCenterWrapper>
-      <EmptyWrapper
-        empty={!testResult?.length}
-        description={
-          <>
-            <Typography.Text strong style={{ display: 'block' }}>
-              There are no tests for this request
-            </Typography.Text>
-            <Typography.Text
-              css={css`
-                font-size: 12px;
-              `}
-              type='secondary'
-            >
-              Write a test script to automate debugging. Learn more about{' '}
-            </Typography.Text>
-            <a
-              href={'https://learning.postman.com/docs/writing-scripts/test-scripts/'}
-              target={'_blank'}
-              rel='noreferrer'
-            >
-              writing tests
-            </a>
-          </>
-        }
-      >
-        {testResult?.map((tr, index) =>
-          tr.passed ? (
-            <div key={index}>
-              <Tag color={token.colorSuccess}>{t('test.passed')}</Tag>
-              <Typography.Text type='secondary' style={{ fontSize: '12px !important' }}>
-                {tr.name}
-              </Typography.Text>
+    <EmptyWrapper
+      empty={!testResult?.length}
+      description={
+        <>
+          <Typography.Text strong style={{ display: 'block' }}>
+            There are no tests for this request
+          </Typography.Text>
+          <Typography.Text
+            css={css`
+              font-size: 12px;
+            `}
+            type='secondary'
+          >
+            Write a test script to automate debugging. Learn more about{' '}
+          </Typography.Text>
+          <a
+            href={'https://learning.postman.com/docs/writing-scripts/test-scripts/'}
+            target={'_blank'}
+            rel='noreferrer'
+          >
+            writing tests
+          </a>
+        </>
+      }
+    >
+      <Space direction='vertical' size='middle' style={{ width: '100%', padding: '8px' }}>
+        {testResult?.map((tr, index) => (
+          <div key={index}>
+            <div style={{ display: 'inline-block', width: '64px', textAlign: 'center' }}>
+              {tr.passed ? (
+                <Tag color={token.colorSuccess}>{t('test.passed')}</Tag>
+              ) : (
+                <Tag color={token.colorError}>{t('test.failed')}</Tag>
+              )}
             </div>
-          ) : (
-            <div key={index}>
-              <Tag color={token.colorError}>{t('test.failed')}</Tag>
-              <Typography.Text type='secondary' style={{ fontSize: '12px !important' }}>
-                {tr.name} | AssertionError: {tr?.error}
-              </Typography.Text>
-            </div>
-          ),
-        )}
-      </EmptyWrapper>
-    </FlexCenterWrapper>
+
+            <Typography.Text type='secondary' style={{ fontSize: '12px !important' }}>
+              {tr.name} {!tr.passed && `| ${tr?.error?.name}: ${tr?.error?.message}`}
+            </Typography.Text>
+          </div>
+        ))}
+      </Space>
+    </EmptyWrapper>
   );
 };
 
