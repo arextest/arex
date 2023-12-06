be9cc5923f4ee4d5372aa8177b0e53ef27f4cb2f
qzmo <qzmo@trip.com>
2023-12-06 19:24:21 +0800

    feat: Dockerfile

--- packages/arex/src/panes/ReplayCaseDetail/CaseDetailTab.tsx
+++ packages/arex/src/panes/ReplayCaseDetail/CaseDetailTab.tsx
@@ -1,8 +1,8 @@
-import { DiffJsonView } from '@arextest/arex-core';
-import { Collapse } from 'antd';
-import React, { FC } from 'react';
+import { DiffJsonView, useTranslation } from '@arextest/arex-core';
+import { Button, Collapse, Descriptions, Modal } from 'antd';
+import React, { FC, useState } from 'react';
 
-import { RecordResult } from '@/services/StorageService';
+import { RecordResult } from '@/services/ReportService';
 
 type CaseDetailTabProps = {
   type: string;
@@ -10,25 +10,87 @@ type CaseDetailTabProps = {
 };
 
 const CaseDetailTab: FC<CaseDetailTabProps> = (props) => {
+  const [detailOpen, setDetailOpen] = useState(false);
+  const [currentDetail, setCurrent] = useState<RecordResult>();
+  const { t } = useTranslation('components');
+
+  const handleClose = () => {
+    setDetailOpen(false);
+  };
   return (
-    <Collapse
-      items={props.compareResults?.map((result, index) => ({
-        key: index,
-        label: <span>{result.operationName}</span>,
-        children: (
-          <DiffJsonView
-            hiddenTooltip
-            height='400px'
-            remark={['Request', 'Response']}
-            diffJson={{
-              left: JSON.stringify(result.targetRequest, null, 2),
-              right: JSON.stringify(result.targetResponse, null, 2),
-            }}
-          />
-        ),
-      }))}
-      style={{ marginTop: '8px' }}
-    />
+    <>
+      <Collapse
+        items={props.compareResults?.map((result, index) => ({
+          key: index,
+          label: (
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+              <span>{result.operationName}</span>
+              <Button
+                size='small'
+                type='text'
+                onClick={(e) => {
+                  // avoid triggering collapse
+                  e.stopPropagation();
+                  setDetailOpen(true);
+                  setCurrent(result);
+                }}
+              >
+                More
+              </Button>
+            </div>
+          ),
+          children: (
+            <DiffJsonView
+              hiddenTooltip
+              height='400px'
+              remark={['Request', 'Response']}
+              diffJson={{
+                left: JSON.stringify(result.targetRequest?.body, null, 2),
+                right: JSON.stringify(result.targetResponse?.body, null, 2),
+              }}
+            />
+          ),
+        }))}
+        style={{ marginTop: '8px' }}
+      />
+      <Modal
+        width='70vw'
+        title={t('caseDetail.caseDetail')}
+        open={detailOpen}
+        footer={null}
+        onCancel={handleClose}
+      >
+        <Descriptions bordered>
+          <Descriptions.Item span={4} label={t('caseDetail.recordId')}>
+            {currentDetail?.recordId}
+          </Descriptions.Item>
+          <Descriptions.Item span={4} label={t('caseDetail.operationName')}>
+            {currentDetail?.operationName}
+          </Descriptions.Item>
+          <Descriptions.Item span={4} label={t('caseDetail.categoryType')}>
+            {currentDetail?.categoryType.name}
+          </Descriptions.Item>
+
+          <Descriptions.Item span={4} label={t('caseDetail.recordVersion')}>
+            {currentDetail?.recordVersion}
+          </Descriptions.Item>
+
+          <Descriptions.Item span={4} label={t('caseDetail.requestAttributes')}>
+            <pre>{JSON.stringify(currentDetail?.targetRequest?.attributes, null, 2)}</pre>
+          </Descriptions.Item>
+          <Descriptions.Item span={4} label={t('caseDetail.requestBodyType')}>
+            {currentDetail?.targetRequest?.type}
+          </Descriptions.Item>
+
+          <Descriptions.Item span={4} label={t('caseDetail.responseAttributes')}>
+            <pre>{JSON.stringify(currentDetail?.targetResponse?.attributes, null, 2)}</pre>
+          </Descriptions.Item>
+          <Descriptions.Item span={4} label={t('caseDetail.responseBodyType')}>
+            {currentDetail?.targetResponse?.type}
+          </Descriptions.Item>
+        </Descriptions>
+      </Modal>
+    </>
   );
 };
 
