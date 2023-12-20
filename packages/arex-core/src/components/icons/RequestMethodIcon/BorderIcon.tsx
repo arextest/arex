6e3f84409061da5c65695d58537cf47a5f0aff09
onePone <pwang31@trip.com>
2023-12-20 14:43:52 +0800

    fix: borderIcon padding

--- packages/arex-core/src/components/icons/RequestMethodIcon/BorderIcon.tsx
+++ packages/arex-core/src/components/icons/RequestMethodIcon/BorderIcon.tsx
@@ -4,7 +4,8 @@ import React, { ReactNode } from 'react';
 export const BorderIcon = styled((props: { icon: ReactNode }) => (
   <span {...props}>{props.icon}</span>
 ))`
-  margin-right: ${(props) => props.theme.marginXS};
+  margin-right: ${(props) => props.theme.marginXS}px;
+  padding: 0 2px 1px;
   border: 1px solid;
   font-size: 12px;
   line-height: 12px;
