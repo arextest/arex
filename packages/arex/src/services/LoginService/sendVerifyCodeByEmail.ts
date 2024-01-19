49cb98937545463e1d858a7cb74a4c4e995c4465
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-01-19 14:25:11 +0800

    feat: update trace url

--- packages/arex/src/services/LoginService/sendVerifyCodeByEmail.ts
+++ packages/arex/src/services/LoginService/sendVerifyCodeByEmail.ts
@@ -2,6 +2,7 @@ import { request } from '@/utils';
 
 export function sendVerifyCodeByEmail(email: string) {
   return request
+    // http://10.144.62.53:8090/api/login/getVerificationCode/123
     .get<{ success: boolean }>(`/webApi/login/getVerificationCode/${email}`, undefined, {
       headers: { 'access-token': 'no' },
     })
