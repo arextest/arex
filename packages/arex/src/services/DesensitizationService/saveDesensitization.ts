ba062576122d556a3677cb3fcd8568e605f03323
onePone <pwang31@trip.com>
2024-02-23 17:39:51 +0800

    refactor: system config

--- packages/arex/src/services/DesensitizationService/saveDesensitization.ts
+++
@@ -1,6 +0,0 @@
-import { request } from '@/utils';
-
-export async function saveDesensitization(params: { jarUrl: string; remark?: string }) {
-  const res = await request.post<boolean>('/webApi/desensitization/saveJar', params);
-  return res.body;
-}
