35eea495eeb39cbd3a09ccaca7962298f51a7009
Nathan Mo <54135657+QizhengMo@users.noreply.github.com>
2024-02-29 14:30:20 +0800

    Refactor/local server (#633)
    * refactor: extract server env

--- packages/arex-server/server.js
+++ packages/arex-server/server.js
@@ -3,15 +3,18 @@ import history from 'connect-history-api-fallback';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 const app = express();
 
-const SERVICE_API_URL = process.env.SERVICE_API_URL || 'http://10.144.62.53:8090';
-const SERVICE_SCHEDULE_URL = process.env.SERVICE_SCHEDULE_URL || 'http://10.144.62.53:8092';
-const SERVICE_STORAGE_URL = process.env.SERVICE_STORAGE_URL || 'http://10.144.62.53:8093';
+const PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 8080
+const SERVICE_API_URL = process.env.SERVICE_API_URL;
+const SERVICE_SCHEDULE_URL = process.env.SERVICE_SCHEDULE_URL;
+const SERVICE_STORAGE_URL = process.env.SERVICE_STORAGE_URL;
+
+if (!SERVICE_API_URL || !SERVICE_SCHEDULE_URL || !SERVICE_STORAGE_URL) {
+  throw new Error('SERVICE_API_URL, SERVICE_SCHEDULE_URL, SERVICE_STORAGE_URL are required');
+}
 
 app.use(
   '/webApi',
@@ -66,11 +69,11 @@ app.use(
   }),
 );
 
-// 健康检查
+// health check
 app.get('/vi/health', (req, res) => {
   res.end(`365ms`);
 });
-// storage
+
 app.get('/env', (req, res) => {
   res.send({
     SERVICE_API_URL,
@@ -79,11 +82,11 @@ app.get('/env', (req, res) => {
   });
 });
 
-app.use(history()); // 这里千万要注意，要在static静态资源上面
-// 托管静态文件
+// should be done before express.static
+app.use(history());
+// redirect to index.html
 app.use(express.static(__dirname + '/dist'));
 
-// 监听 8080 端口
-app.listen(8080, function () {
-  console.log('hi');
+app.listen(PORT, function () {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
