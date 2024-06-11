399cbe58894ffb094034aaf3933bf77aabb2572a
Allen Zhang <wr_zhang25@163.com>
2024-06-11 14:35:54 +0800

    feat: update sentry (#683)

--- packages/arex/src/utils/sentry.ts
+++ packages/arex/src/utils/sentry.ts
@@ -12,4 +12,5 @@ export const initSentry = () => {
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
+  Sentry.setTag('user', localStorage.getItem('email') || 'anonymous');
 };
