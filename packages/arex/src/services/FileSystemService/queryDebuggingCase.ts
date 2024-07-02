0343400bc67faaa9f682a7a2d117b66d1fa7adb0
Jesse Rodriguez <54310591+1pone@users.noreply.github.com>
2024-07-02 11:29:00 +0800

    fix: body nullable (#697)
    * fix: body nullable
    
    * feat: add trans
    
    ---------
    
    Co-authored-by: onePone <pwang31@trip.com>

--- packages/arex/src/services/FileSystemService/queryDebuggingCase.ts
+++ packages/arex/src/services/FileSystemService/queryDebuggingCase.ts
@@ -1,19 +1,10 @@
 import { request } from '@/utils';
-// case debug时预处理请求头，返回 ‘application/json’ 或 ‘multipart/form-data’
-const preHandlerHeaders = (contentType = '') => {
-  if (contentType.includes('application/json')) {
-    return 'application/json';
-  } else if (contentType.includes('multipart/form-data')) {
-    return 'multipart/form-data';
-  } else {
-    return contentType;
-  }
-};
-export async function queryDebuggingCase(params: { recordId: string; planId?: string }) {
-  return request
-    .get<any>(`/webApi/filesystem/queryDebuggingCase/${params.planId}/${params.recordId}`)
-    .then((res) => {
-      res.body.body.contentType = preHandlerHeaders(res.body.body.contentType);
-      return res;
-    });
+
+export async function queryDebuggingCase(params: {
+  recordId: string;
+  planId?: string;
+}): Promise<any> {
+  return request.get<any>(
+    `/webApi/filesystem/queryDebuggingCase/${params.planId}/${params.recordId}`,
+  );
 }
