aa2e9d46032c8436d3972364341757eafe8a4c3b
Jesse Rodriguez <wellpeng97@gmail.com>
2024-07-22 20:40:48 +0800

    fix: request client variable

--- packages/arex-request/src/vite-env.d.ts
+++ packages/arex-request/src/vite-env.d.ts
@@ -6,5 +6,6 @@ declare global {
     message: MessageInstance;
     __AREX_EXTENSION_INSTALLED__: boolean; // 是否安装了arex-chrome-extension
     __AREX_EXTENSION_VERSION__: string; // arex-chrome-extension 最新版本号
+    electron?: any;
   }
 }
